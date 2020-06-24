cdbde5c646db8fc03a7d93235edb1189c27f4228
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-24 13:06:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,14 @@
         }
         var j = 0;
         while (j < B.length) {
+            var check = false;
             for (var k = 0; k < res.length; k++) {
-                if (res[k] != B[j]) {
-                    res[res.length + 1] = B[j];
+                if (res[k] == B[j]) {
+                    check = true;
                 }
+            }
+            if (check == false) {
+                res[res.length + 1] = B[j];
             }
             j++;
         }
