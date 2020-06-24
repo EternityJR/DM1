8fc57e9617475c89b0d6fb616e1cc0c7898d63e0
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-24 14:55:33 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-    function Union(A, B) {
-        var res = [];
-        for (var i = 0; i < A.length; i++) {
-            res[i] = A[i];
-        }
-        var j = 0;
-        while (j < B.length) {
-            var check = false;
-            for (var k = 0; k < res.length; k++) {
-                if (res[k] == B[j]) {
-                    check = true;
-                }
-            }
-            if (check == false) {
-                res[res.length + 1] = B[j];
-            }
-            j++;
-        }
-        return "Результат объединения множеств" + res;
-    }
+/*
+    *проверка на соответствие типа
+    *
+    *проверка каждого символа на соответствие
+    *
+    *@глобальные переменные res, str
+    *@param array
+    *return res
+*/
 function validate(array) {
     var res = true;
     var str = "";
@@ -42,8 +31,7 @@ function validate(array) {
     
 }
 
-function getDataUser() {
-    var func = 0;
+function getDataUser(func) {
     var A = document.getElementById("arrayA").value;
     A = A.split(" ");
     if (!validate(A)) return;
@@ -67,3 +55,100 @@ function getDataUser() {
     else
       document.getElementById("result").innerHTML = Negating(A, U);
 }
+
+    /*
+    *объединение множеств
+    *
+    *вывод всех уникальных значений из обоих множеств
+    *
+    *@глобальные переменные 
+    *@param A, B
+    *return res
+    */
+    function Union(A, B) {
+        var res = [];
+        for (var i = 0; i < A.length; i++) {
+            res[i] = A[i];
+        }
+        var j = 0;
+        while (j < B.length) {
+            var check = false;
+            for (var k = 0; k < res.length; k++) {
+                if (res[k] == B[j]) {
+                    check = true;
+                }
+            }
+            if (check == false) {
+                res[res.length] = B[j];
+            }
+            j++;
+        }
+        return "Результат объединения множеств: " + res;
+    }
+
+    /*
+    *пересечение множеств
+    *
+    *вывод всех уникальных значений, которые встречаются одновременно в двух заданных множествах
+    *
+    *@глобальные переменные 
+    *@param A, B
+    *return res
+    */
+    function Intersection(A, B) {
+        var res = [];
+        for (var i = 0; i < A.length; i++) {
+            for (var j = 0; j < B.length; j++) {
+                if (A[i] != B[j]){
+                    res[i] = A[i];
+                }
+            }
+        }
+        return "Результат пересечения множеств: " + res;
+    }
+
+    /*
+    *дополнение множеств
+    *
+    *вывод всех уникальных значений из множества A и значений из множества B, которые ещё не встречались
+    *
+    *@глобальные переменные 
+    *@param A, B
+    *return res
+    */
+    function Addition(A, B) {
+      
+    }
+
+    /*
+    *симметрическая разность
+    *
+    *вывод всех уникальных значений из множеств A и B, кроме тех, что встречаются в обоих
+    *
+    *@глобальные переменные 
+    *@param A, B
+    *return res
+    */
+    function Difference(A, B) {
+
+        var diff=[];
+        for (var i=0; i < A.length; i++) {
+            if (B.indexOf(A[i]) <= -1 ) { 
+                result.push(A[i]);
+            }
+        }
+        return "Результат симметрической разности:" + diff;
+    }
+
+    /*
+    *отрицание множества А относительно универсального множества
+    *
+    *вывод всех значений, которые входят в универсальное множество U, кроме тех, что входят в множество А
+    *
+    *@глобальные переменные 
+    *@param A, U
+    *return res
+    */
+    function Negating(A, U) {
+        
+    }
