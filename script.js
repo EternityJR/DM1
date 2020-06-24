78042e6e1439251de72a379e1eee2a0ed14aa8f1
EternityJR <65914566+EternityJR@users.noreply.github.com>
2020-06-24 10:22:16 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+    function Union(A, B) {
+        var res = [];
+        for (var i = 0; i < A.length; i++) {
+            res[i] = A[i]j
+        }
+        for (var i = A.length; i < A.length + B.length; i++) {
+            for (var j = 0; j < B.length; i++)
+            res [i] = B[j];
+        }
+        return "Результат объединения множеств" + res;
+    }
